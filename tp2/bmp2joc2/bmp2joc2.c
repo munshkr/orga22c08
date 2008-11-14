9ccef56b36d82df25866ed2c3f41235e0999ff11
DarthPolly <DarthPolly@localhost>
2008-11-14 01:44:13 +0000

    Pocos cambios. ? Agregu? el compilador para 64 bits porque lo vengo reescribiendo siempre. ? En bmp2joc2.c escrib? un par de validaciones y un comentario que estar?a bueno que revisemos. Por ahi no es muy importante, pero como optimizaci?n puede llegar a ser interesante.

--- tp2/bmp2joc2/bmp2joc2.c
+++ tp2/bmp2joc2/bmp2joc2.c
@@ -1,79 +1,113 @@
-// bmp2oc2
-// Lee un archivo en formato bmp, lo codifica con el algoritmo Huffman y lo
-// escribe en un archivo en formato oc2.
+// bmp2joc2
 
 #include <stdlib.h>
-
 #include "readbmp.h"
-// #include "writeoc2.h"
+//#include "writeoc2.h"
 #include "writecbmp.h"
 
-extern int* calcularApariciones(char* buffer, int size);
-extern int armarTablaCodigos(int* tablaApariciones, codificacion** tablaCodigos);
-extern long long int codificar (char* buffer, int buf_size, codificacion* tc, int tc_size, int* ta, char** bitstream);
-
+//extern int* calcularApariciones(char* buffer, int size);
+//extern int armarTablaCodigos(int* tablaApariciones, codificacion** tablaCodigos);
+//extern long long int codificar (char* buffer, int buf_size, codificacion* tc, int tc_size, int* ta, char** bitstream);
 
 /*
-    Idea vaga de como seria este programa:
+	readbmp(archivo_fuente, &fh, &ih, &RBuffer, &GBuffer, &BBuffer);
 
-    BMPfileheader fh;
-    BMPinfoheader ih;
+	short r_bloque[8][8];
+	short g_bloque[8][8];
+	short b_bloque[8][8];
 
-    char* buffer = readbmp(argv[0] , &fh, &ih);
+	float dct[8][8] = generarDCT();
 
-    int bufsize = fh.Size - 54
+	// canal R
+	for (i=0;i<ancho/8;i++){
+		for (j=0;j<alto/8;j++){
+			r_bloque = dividirEnBloques(RBuffer, i, j);
 
-    TablaApariciones* ta = calcularApariciones(buffer, bufsize);
+		}
+	}
 
-    TablaCodigos* tc = armarTablaCodigos(ta);
-
-    char* bitstream = codificar(tc, buffer);
-
-    int todoOK = writeoc2(fh, ih, tc, bitstream);
 
 */
 
-
-
 int main (int argc, char* argv[]) {
+	int bOK = 1;
 
-	// si no recibe los 2 parametros, termina.
-	if (argc != 3) {
+	if (argc != 3) {													// si no recibe los 2 parametros, termina.
 		printf("Numero de parametros incorrecto.\n");
 		printf("Uso:  bmp2joc2 FUENTE DESTINO\n");
         printf("\n");
         return EXIT_FAILURE;
 	}
 
-	unsigned int k;
-
     char* archivo_fuente = argv[1];
-//    archivo_fuente = "../res/nhouse.bmp";
-
     char* archivo_destino = argv[2];
 
-    // declara las estructuras de los headers del archivo bmp fuente.
-    BMPfileheader fh;
-    BMPinfoheader ih;
+    BMPfileheader fh;													// declara las estructuras de los
+    BMPinfoheader ih;													// headers del archivo bmp fuente.
 
-    // abre el archivo bmp, levanto los headers y carga los datos
-    // de la imagen en un buffer.
+    char* RBuffer;														// abre el archivo bmp, levanto los
+	char* GBuffer;														// headers y carga los datos de la
+	char* BBuffer;														// imagen en los buffer de cada canal (RGB).
 
+	bOK = readbmp(archivo_fuente, &fh, &ih, &RBuffer, &GBuffer, &BBuffer)
+		//writecbmp(archivo_destino, &fh, &ih, RBuffer, GBuffer, BBuffer);
 
-	char* RBuffer;
-	char* GBuffer;
-	char* BBuffer;
-
-	if (readbmp(archivo_fuente, &fh, &ih, &RBuffer, &GBuffer, &BBuffer)) {
-
-		writecbmp(archivo_destino, &fh, &ih, RBuffer, GBuffer, BBuffer);
-
+	if (bOK) {
+		if ((ih->Height % 8 == 0) && (ih->Width % 8 == 0)) {
+			printf("Las medidas de la imágen no son múltiplos de 8.\n");
+			bOK = false;
+		}
 	}
 
-    free(RBuffer);
+    if (bOK) {
+    	int bloquesAncho = ih->Width / 8;
+    	int bloquesAlto = ih->Height / 8;
+
+    	/*
+    	NOTA:
+    	Extraer las matrices de 8x8 de un buffer del color que sea, es equivalente a ordenar las tríadas de bytes.
+
+    	B = Byte
+
+		   A	    B		 C		  D		   E		F		 G
+
+1    	BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB
+2    	BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB
+3    	BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB
+4    	BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB
+5    	BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB
+6    	BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB
+7    	BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB
+8    	BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB
+
+9    	BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB
+10    	BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB
+11   	BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB
+12    	BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB
+13    	BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB
+14    	BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB
+15    	BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB
+16    	BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB BBBBBBBB
+
+    	El nuevo orden debería ser
+
+    	A1		A2		A3		A4		A5		A6		A7
+    	A8		B1		B2		B3		B4		B5		B6
+    	B7		B8		C1		C2		..		..		..
+    	..		..		..		..		..		..		..
+
+    	y asi...
+
+    	Tal vez haya algo que podamos hacer para ordenar en el mismo buffer y entonces no tener que pedir memoria.
+    	*/
+
+    }
+
+
+	free(RBuffer);
     free(GBuffer);
     free(BBuffer);
 
-    return EXIT_SUCCESS;
 
+    return EXIT_SUCCESS;
 }
--- tp2/bmp2joc2/bmp2joc2.h
+++ tp2/bmp2joc2/bmp2joc2.h
@@ -1,13 +1,8 @@
-#ifndef __BMP2OC2_H__
-#define __BMP2OC2_H__
+#ifndef __BMP2JOC2_H__
+#define __BMP2JOC2_H__
 
-#define TC_ROW_SIZE 8
-
-typedef struct {
-    char simbolo;
-    char longCod;
-    int cod;
-} codificacion;
+#define false 0
+#define true 1
 
 typedef struct _BMPfileheader {
     char MagicWord1;  // char "B"
@@ -32,59 +27,5 @@ typedef struct _BMPinfoheader {
     int ClrImportant;
 } BMPinfoheader;
 
-typedef struct _OC2fileheader {
-	int fType;			// el string "OC2" -> 0x4f433200 (en little-endian)
-	short tcSize;		// tamaño en bytes de la tabla de codigos
-	long long int bsSize;	// tamaño en bits del bitstream
-} OC2fileheader;
 
-
-//
-// accepts a decimal integer and returns a binary coded string
-//
-void dec2bin(long decimal, int longCod, char *binary)
-{
-  int  k = 0, n = 0;
-  int  neg_flag = 0;
-  int  remain;
-  int  old_decimal;  // for test
-  char temp[80];
-
-  int j = 0;
-  while(j<32){
-  	temp[j] = '0';
-  	j++;
-  }
-
-  // take care of negative input
-  if (decimal < 0)
-  {      
-    decimal = -decimal;
-    neg_flag = 1;
-  }
-  do 
-  {
-    old_decimal = decimal;   // for test
-    remain    = decimal % 2;
-    // whittle down the decimal number
-    decimal   = decimal / 2;
-    // this is a test to show the action
-    //printf("%d/2 = %d  remainder = %d\n", old_decimal, decimal, remain);
-    // converts digit 0 or 1 to character '0' or '1'
-    temp[k++] = remain + '0';
-  } while (decimal > 0);
-
-  if (neg_flag)
-    temp[k++] = '-';       // add - sign
-  else
-    temp[k++] = '0';       // space
-
-  // reverse the spelling
-  k = longCod;
-  while (k >= 0)
-    binary[n++] = temp[--k];
-
-  binary[n-1] = 0;         // end with NULL
-}
-
-#endif // __BMP2OC2_H__
+#endif // __BMP2JOC2_H__
