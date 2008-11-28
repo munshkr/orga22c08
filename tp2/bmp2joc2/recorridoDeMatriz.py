44797a32411cd3412646c50ead5d2bd887c4f43c
colmeiro <colmeiro@localhost>
2008-11-28 11:24:09 +0000

    Ya est? listo en python el comprimir y descomprimir. Est? uno a continuacion del otro en el mismo archivo, toma un bloque trabajado posta (el ultimo de caja.bmp), lo codifica y lo decodifica.

--- tp2/bmp2joc2/recorridoDeMatriz.py
+++ tp2/bmp2joc2/recorridoDeMatriz.py
@@ -9,11 +9,14 @@ matriz = [[125, -1, 0, 0, 0, 0, 0, 0],
 [0, 0, 0, 0, 0, 0, 0, 0],
 [0, 0, 0, 0, 0, 0, 0, 25704]
 ]
+
+
 print "Matriz:"
 for h in range(8):
+	# matriz[h] = range(1,9) #si quieren ver algo que comprime POCO, sacar el numeral de principio de linea
 	print matriz[h]
 
-print "Recorrido: "
+print "Stream Codificado: "
 codStream = []
 cantCeros = 0
 primero = True
@@ -22,11 +25,9 @@ for i in range(8):
 		if i % 2 == 0:
 			row = i-j
 			col = j
-			#print i-j, j, matriz[i][j]
 		else:
 			row = j
 			col = i-j
-			#print j, i-j, matriz[i][j]
 		valor = matriz[row][col]
 		if valor != 0:
 			if not(primero):
@@ -36,7 +37,7 @@ for i in range(8):
 				primero = False
 			codStream.append(valor)
 			print valor,
-			cantceros = 0
+			cantCeros = 0
 		else:
 			cantCeros = cantCeros + 1
 for j in range(7):
@@ -44,11 +45,9 @@ for j in range(7):
 		if j % 2 == 0:
 			row = i
 			col = 8+j-i
-			#print i, 8+j-i, matriz[i][j]
 		else:
 			row = 8+j-i
 			col = i
-			#print 8+j-i, i, matriz[i][j]
 		valor = matriz[row][col]
 		if valor != 0:
 			codStream.append(cantCeros)
@@ -63,8 +62,11 @@ print 0, 0
 
 print "\nStream Codificado:"
 print codStream
-print "Recorrido:"
-matriz = [[667]*8]*8
+
+print "Matriz Decodificada:"
+matriz=[]
+for i in range(8):
+	matriz.append([1]*8)
 matriz[0][0] = codStream[0]
 cantCeros = codStream[1]
 streamAct = 2
@@ -78,7 +80,7 @@ for i in range(8):
 			row = j
 			col = i-j
 		if cantCeros > 0:
-			matriz[row][col] = 22
+			matriz[row][col] = 0
 			cantCeros = cantCeros - 1
 		else:
 			if primero:
@@ -101,11 +103,10 @@ for j in range(7):
 			matriz[row][col] = 0
 			cantCeros = cantCeros - 1
 		else:
-			if primero:
-				primero = False
-			else:
-				matriz[row][col] = codStream[streamAct]
-				streamAct = streamAct+1
-				cantCeros = codStream[streamAct]
-				streamAct = streamAct+1
-print matriz
+			matriz[row][col] = codStream[streamAct]
+			streamAct = streamAct+1
+			cantCeros = codStream[streamAct]
+			streamAct = streamAct+1
+
+for h in range(8):
+	print matriz[h]
